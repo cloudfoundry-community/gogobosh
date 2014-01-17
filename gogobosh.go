7e1e1193a2d3fcf8ba193e8199b6ac4a099c42b6
Dr Nic Williams <drnicwilliams@gmail.com>
2014-01-17 15:28:50 -0800

    nested structs for parsing GetStatusResponse

--- gogobosh.go
+++ gogobosh.go
@@ -19,6 +19,54 @@ type Director struct {
 	SnapshotsEnabled bool
 }
 
+type GetStatusResponse struct {
+	Name string          `json:"name"`
+	UUID string          `json:"uuid"`
+	Version string       `json:"version"`
+	User string          `json:"user"`
+	CPI string           `json:"cpi"`
+	Features getStatusFeaturesResponse `json:"features"`
+}
+
+type getStatusFeaturesResponse struct {
+	Dns getStatusFeaturesDNS                                   `json:"dns"`
+	CompiledPackageCache getStatusFeaturesCompiledPackageCache `json:"compiled_package_cache"`
+	Snapshots getStatusFeaturesSnapshots                       `json:"snapshots"`
+}
+
+type getStatusFeaturesDNS struct {
+	Status bool                       `json:"status"`
+	Extras getStatusFeaturesDNSExtras `json:"extras"`
+}
+
+type getStatusFeaturesDNSExtras struct {
+	DomainName string `json:"domain_name"`
+}
+
+type getStatusFeaturesCompiledPackageCache struct {
+	Status bool                                        `json:"status"`
+	Extras getStatusFeaturesCompiledPackageCacheExtras `json:"extras"`
+}
+
+type getStatusFeaturesCompiledPackageCacheExtras struct {
+	Provider string `json:"provider"`
+}
+
+type getStatusFeaturesSnapshots struct {
+	Status bool `json:"status"`
+}
+
+func (resource GetStatusResponse) ToModel() (director Director) {
+/*	director.ApplicationFields = resource.ToFields()
+	routes := []cf.RouteSummary{}
+	for _, route := range resource.Routes {
+		routes = append(routes, route.ToModel())
+	}
+	app.RouteSummaries = routes
+
+*/	return
+}
+
 func NewDirector(targetURL string, username string, password string) (director Director) {
 	config := DirectorConfig{}
 	config.targetURL = targetURL
--- gogobosh_test.go
+++ gogobosh_test.go
@@ -1,10 +1,44 @@
 package gogobosh_test
 
 import (
-/*	. "github.com/cloudfoundry-community/gogobosh"*/
+	gogobosh "github.com/cloudfoundry-community/gogobosh"
 	. "github.com/onsi/ginkgo"
-/*	. "github.com/onsi/gomega"*/
+	. "github.com/onsi/gomega"
+	"encoding/json"
 )
 
-var _ = Describe("Gamble", func() {
+var _ = Describe("GoGoBOSH", func() {
+	It("parse response", func() {
+		responseJSON := `{
+		  "name": "Bosh Lite Director",
+		  "uuid": "bd462a15-213d-448c-aa5b-66624dad3f0e",
+		  "version": "1.5.0.pre.1657 (14bc162c)",
+		  "user": "admin",
+		  "cpi": "warden",
+		  "features": {
+		    "dns": {
+		      "status": false,
+		      "extras": {
+		        "domain_name": "bosh"
+		      }
+		    },
+		    "compiled_package_cache": {
+		      "status": true,
+		      "extras": {
+		        "provider": "local"
+		      }
+		    },
+		    "snapshots": {
+		      "status": false
+		    }
+		  }
+		}`
+		resource := new(gogobosh.GetStatusResponse)
+		b := []byte(responseJSON)
+		err := json.Unmarshal(b, &resource)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(resource.Name).To(Equal("Bosh Lite Director"))
+		Expect(resource.UUID).To(Equal("bd462a15-213d-448c-aa5b-66624dad3f0e"))
+		Expect(resource.User).To(Equal("admin"))
+	})
 })
