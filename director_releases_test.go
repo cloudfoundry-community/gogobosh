3b8ed3fbd7b7a8f464103c37633bba836d6fac6a
Dr Nic Williams <drnicwilliams@gmail.com>
2014-01-19 10:53:27 -0800

    Test /releases => []DirectorRelease{}

--- director_releases_test.go
+++ director_releases_test.go
@@ -5,9 +5,70 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"encoding/json"
+	"net/http"
 )
 
 var _ = Describe("get list of releases", func() {
+	It("GET /releases to return []DirectorRelease{}", func() {
+		request := gogobosh.NewDirectorTestRequest(gogobosh.TestRequest{
+			Method: "GET",
+			Path:   "/releases",
+			Response: gogobosh.TestResponse{
+				Status: http.StatusOK,
+				Body: `[
+				  {
+				    "name": "cf",
+				    "release_versions": [
+				      {
+				        "version": "153",
+				        "commit_hash": "009fdd9a",
+				        "uncommitted_changes": true,
+				        "currently_deployed": true,
+				        "job_names": [
+				          "cloud_controller_ng",
+				          "nats",
+				          "dea_next",
+				          "login",
+				          "health_manager_next",
+				          "uaa",
+				          "debian_nfs_server",
+				          "loggregator",
+				          "postgres",
+				          "dea_logging_agent",
+				          "syslog_aggregator",
+				          "narc",
+				          "haproxy",
+				          "hm9000",
+				          "saml_login",
+				          "nats_stream_forwarder",
+				          "collector",
+				          "pivotal_login",
+				          "loggregator_trafficcontroller",
+				          "etcd",
+				          "gorouter"
+				        ]
+				      }
+				    ]
+				  }
+				]`}})
+		ts, handler, repo := createDirectorRepo(request)
+		defer ts.Close()
+
+		releases, apiResponse := repo.GetReleases()
+
+		release := releases[0]
+		Expect(release.Name).To(Equal("cf"))
+
+		release_version := release.Versions[0]
+		Expect(release_version.Version).To(Equal("153"))
+		Expect(release_version.CommitHash).To(Equal("009fdd9a"))
+		Expect(release_version.UncommittedChanges).To(Equal(true))
+		Expect(release_version.CurrentlyDeployed).To(Equal(true))
+
+		Expect(apiResponse.IsSuccessful()).To(Equal(true))
+		Expect(handler.AllRequestsCalled()).To(Equal(true))
+	})
+
 	/*
 	 * To get the director info:
 	 *   curl -k -u admin:admin https://192.168.50.4:25555/releases
