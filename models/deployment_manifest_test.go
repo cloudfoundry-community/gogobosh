8b3e0a07decf343f145eb94edb3e1f9644b30b91
Dr Nic Williams <drnicwilliams@gmail.com>
2014-10-17 16:49:43 -0400

    DeploymentManifest#FindJobTemplates added

---
+++ models/deployment_manifest_test.go
@@ -0,0 +1,23 @@
+package models_test
+
+import (
+	"github.com/cloudfoundry-community/gogobosh/models"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("DeploymentManifest", func() {
+	It("FindJobTemplates", func() {
+		manifest := &models.DeploymentManifest{
+			Jobs: []*models.ManifestJob{
+				{Name: "job1", JobTemplates: []*models.ManifestJobTemplate{{Name: "common"}}},
+				{Name: "job2", JobTemplates: []*models.ManifestJobTemplate{{Name: "common"}}},
+				{Name: "other", JobTemplates: []*models.ManifestJobTemplate{{Name: "other"}}},
+			},
+		}
+		jobs := manifest.FindJobTemplates("common")
+		Expect(len(jobs)).To(Equal(2))
+
+	})
+})
