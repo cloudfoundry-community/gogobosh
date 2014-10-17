022c2473d5e46642689b701873b460dd50c930c8
Dr Nic Williams <drnicwilliams@gmail.com>
2014-10-17 16:21:48 -0400

    set of structs representing a DeploymentManifest

--- gogobosh.go
+++ gogobosh.go
@@ -53,7 +53,68 @@ type Deployment struct {
 
 // DeploymentManifest describes all the configuration for any BOSH deployment
 type DeploymentManifest struct {
-	Name string
+	Meta          map[string]interface{}
+	Name          string
+	DirectorUUID  string `yaml:"director_uuid"`
+	Releases      []*NameVersion
+	Compilation   *manifestCompilation
+	Update        *manifestUpdate
+	Networks      []*manifestNetwork
+	ResourcePools []*manifestResourcePool `yaml:"resource_pools"`
+	Jobs          []*manifestJob
+	Properties    *map[string]interface{}
+}
+
+type manifestCompilation struct {
+	Workers             int                     `yaml:"workers"`
+	NetworkName         string                  `yaml:"network"`
+	ReuseCompilationVMs bool                    `yaml:"reuse_compilation_vms"`
+	CloudProperties     *map[string]interface{} `yaml:"cloud_properties"`
+}
+
+type manifestUpdate struct {
+	Canaries        int
+	MaxInFlight     int    `yaml:"max_in_flight"`
+	CanaryWatchTime string `yaml:"canary_watch_time"`
+	UpdateWatchTime string `yaml:"update_watch_time"`
+	Serial          bool
+}
+
+type manifestNetwork struct {
+	Name            string
+	Type            string
+	CloudProperties *map[string]interface{} `yaml:"cloud_properties"`
+	Subnets         interface{}
+}
+
+type manifestResourcePool struct {
+	Name            string
+	NetworkName     string `yaml:"network"`
+	Stemcell        string
+	CloudProperties string `yaml:"cloud_properties"`
+}
+
+type manifestJob struct {
+	Name             string
+	Templates        []*manifestJobTemplate
+	Instances        int             `yaml:"instances,omitempty"`
+	ResourcePoolName string          `yaml:"resource_pool"`
+	PersistentDisk   int             `yaml:"persistent_disk,omitempty"`
+	Lifecycle        string          `yaml:"lifecycle,omitempty"`
+	Update           *manifestUpdate `yaml:"update"`
+	Networks         []*manifestJobNetwork
+	Properties       *map[string]interface{} `yaml:"properties"`
+}
+
+type manifestJobTemplate struct {
+	Name    string
+	Release string
+}
+
+type manifestJobNetwork struct {
+	Name      string
+	Default   *[]string `yaml:"default"`
+	StaticIPs *[]string `yaml:"static_ips"`
 }
 
 // DeploymentVM describes the association of a running server
